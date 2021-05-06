b1414fced4bf140e3f351c9769ffcb6544bca833
Victor Ma <vma@redhat.com>
2021-05-06 17:33:44 -0400

    Add all the other events currently in the repo

--- src/main/java/com/redhat/jfr/tests/event/TestDiverseEvents.java
+++ src/main/java/com/redhat/jfr/tests/event/TestDiverseEvents.java
@@ -1,6 +1,10 @@
 package com.redhat.jfr.tests.event;
 
 import com.redhat.jfr.events.StringEvent;
+import com.redhat.jfr.events.TimestampedEvent;
+import com.redhat.jfr.events.annotated.EnabledEvent;
+import com.redhat.jfr.events.annotated.RegisteredEvent;
+import com.redhat.jfr.events.annotated.UnregisteredEvent;
 import com.redhat.jfr.utils.JFR;
 import com.redhat.jfr.utils.LocalJFR;
 import com.redhat.jfr.utils.Stressor;
@@ -14,22 +18,35 @@ public class TestDiverseEvents {
         // TestMultipleEvents
         for (int i = 0; i < COUNT; i++) {
             StringEvent event = new StringEvent();
-            event.message = "Event has been generated!";
+            event.message = "StringEvent has been generated as part of TestMultipleEvents!";
             event.commit();
         }
-
 
         // TestConcurrentEvents
         int threadCount = 8;
         Runnable r = () -> {
             for (int i = 0; i < COUNT; i++) {
                 StringEvent event = new StringEvent();
-                event.message = "Event has been generated!";
+                event.message = "StringEvent has been generated as part of TestConcurrentEvents!";
                 event.commit();
             }
         };
         Thread.UncaughtExceptionHandler eh = (t, e) -> e.printStackTrace();
         Stressor.execute(threadCount, eh, r);
+
+        // TimestampedEvent
+        TimestampedEvent timestampedEvent = new TimestampedEvent();
+        timestampedEvent.message = "TimestampedEvent has been generated!";
+        timestampedEvent.commit();
+
+        // EnabledEvent (@Enabled(false))
+        new EnabledEvent().commit();
+
+        // RegisteredEvent (@Enabled(false))
+        new RegisteredEvent().commit();
+
+        // UnregisteredEvent (@Enabled(false))
+        new UnregisteredEvent().commit();
 
 
         long d0 = System.currentTimeMillis() - s0;
