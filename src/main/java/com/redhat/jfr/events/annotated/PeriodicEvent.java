e728dc71fe1b0272889644a26bfcbb094be59028
Victor Ma <vma@redhat.com>
2021-05-12 12:26:14 -0400

    Add PeriodicEvent

---
+++ src/main/java/com/redhat/jfr/events/annotated/PeriodicEvent.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2021, Red Hat, Inc.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This file is part of the Red Hat GraalVM Testing Suite (the suite).
+ *
+ * The suite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ * The suite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the suite.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.redhat.jfr.events.annotated;
+
+import jdk.jfr.Event;
+import jdk.jfr.Label;
+import jdk.jfr.Period;
+import jdk.jfr.Description;
+
+@Label("Period Event")
+@Period("beginChunk")
+@Description("Emitted at the beginning of every chunk")
+public class PeriodicEvent extends Event {
+    public long time;
+}
