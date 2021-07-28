6fc9da7f993db443d477a6deb21b8b8f6d5e5279
Victor Ma <vma@redhat.com>
2021-07-28 11:39:28 -0400

    Add ClassEvent and IntegerEvent

---
+++ src/main/java/com/redhat/jfr/events/ClassEvent.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021, Red Hat, Inc.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This file is part of the Red Hat GraalVM Testing Suite (the suite).
+ *
+ * The suite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ * The suite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the suite.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.redhat.jfr.events;
+
+import jdk.jfr.Description;
+import jdk.jfr.Event;
+import jdk.jfr.Label;
+import jdk.jfr.StackTrace;
+
+@Label("Class Event")
+@Description("An event with a class payload")
+@StackTrace(false)
+public class ClassEvent extends Event {
+
+    @Label("Clazz")
+    public Class clazz;
+}
--- src/main/java/com/redhat/jfr/tests/event/TestConcurrentEvents.java
+++ src/main/java/com/redhat/jfr/tests/event/TestConcurrentEvents.java
@@ -1,6 +1,8 @@
 package com.redhat.jfr.tests.event;
 
 import com.redhat.jfr.events.StringEvent;
+import com.redhat.jfr.events.IntegerEvent;
+import com.redhat.jfr.events.ClassEvent;
 import com.redhat.jfr.utils.JFR;
 import com.redhat.jfr.utils.LocalJFR;
 import com.redhat.jfr.utils.Stressor;
@@ -12,15 +14,24 @@ public class TestConcurrentEvents {
     public static void main(String[] args) throws Exception {
         long s0 = System.currentTimeMillis();
         JFR jfr = new LocalJFR();
-        int threadCount = 8;
+        // threadCount set to # of cores of ibm-x3650m4-01-vm-05.ibm2.lab.eng.bos.redhat.com
+        int threadCount = 2;
         long id = jfr.startRecording("TestConcurrentEvents");
 
         Runnable r = () -> {
             int count = 1024 * 1024;
             for (int i = 0; i < count; i++) {
-                StringEvent event = new StringEvent();
-                event.message = "Event has been generated!";
-                event.commit();
+                StringEvent stringEvent = new StringEvent();
+                stringEvent.message = "Event has been generated!";
+                stringEvent.commit();
+
+                IntegerEvent integerEvent = new IntegerEvent();
+                integerEvent.number = Integer.MAX_VALUE;
+                integerEvent.commit();
+
+                ClassEvent classEvent = new ClassEvent();
+                classEvent.clazz = Math.class;
+                classEvent.commit();
             }
         };
         Thread.UncaughtExceptionHandler eh = (t, e) -> e.printStackTrace();
