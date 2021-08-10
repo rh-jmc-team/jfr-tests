c20908bb1ef40c8cab395cda8cb1bdcba96509c3
Victor Ma <vma@redhat.com>
2021-08-10 15:16:01 -0400

    Add TestConcurrentEventsInteger

---
+++ src/main/java/com/redhat/jfr/tests/event/TestConcurrentEventsInteger.java
@@ -0,0 +1,35 @@
+package com.redhat.jfr.tests.event;
+
+import com.redhat.jfr.events.IntegerEvent;
+import com.redhat.jfr.utils.JFR;
+import com.redhat.jfr.utils.LocalJFR;
+import com.redhat.jfr.utils.Stressor;
+
+import java.io.File;
+
+public class TestConcurrentEventsInteger {
+
+    public static void main(String[] args) throws Exception {
+        long s0 = System.currentTimeMillis();
+        JFR jfr = new LocalJFR();
+        int threadCount = 8;
+        long id = jfr.startRecording("TestConcurrentEventsInteger");
+
+        Runnable r = () -> {
+            int count = 1024 * 1024;
+            for (int i = 0; i < count; i++) {
+                IntegerEvent integerEvent = new IntegerEvent();
+                integerEvent.number = Integer.MAX_VALUE;
+                integerEvent.commit();
+            }
+        };
+        Thread.UncaughtExceptionHandler eh = (t, e) -> e.printStackTrace();
+        Stressor.execute(threadCount, eh, r);
+
+        File recording = jfr.endRecording(id);
+
+        long d0 = System.currentTimeMillis() - s0;
+        System.out.println("elapsed:" + d0);
+        System.err.println("jfr recording: " + recording);
+    }
+}
---
+++ src/main/java/com/redhat/jfr/tests/event/TestConcurrentEventsIntegerClean.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2021, Red Hat, Inc.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This file is part of the Red Hat GraalVM Testing Suite (the suite).
+ *
+ * The suite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ * The suite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the suite.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.redhat.jfr.tests.event;
+
+import com.redhat.jfr.events.IntegerEvent;
+import com.redhat.jfr.utils.Stressor;
+
+public class TestConcurrentEventsIntegerClean {
+    private static final int COUNT = 1024 * 1024;
+
+    public static void main(String args[]) throws Exception {
+        long s0 = System.currentTimeMillis();
+        run();
+        long d0 = System.currentTimeMillis() - s0;
+        System.out.println("elapsed:" + d0);
+    }
+
+    public static void run() throws Exception {
+        int threadCount = 8;
+        Runnable r = () -> {
+            for (int i = 0; i < COUNT; i++) {
+                IntegerEvent integerEvent = new IntegerEvent();
+                integerEvent.number = Integer.MAX_VALUE;
+                integerEvent.commit();
+            }
+        };
+        Thread.UncaughtExceptionHandler eh = (t, e) -> e.printStackTrace();
+        Stressor.execute(threadCount, eh, r);
+    }
+}
